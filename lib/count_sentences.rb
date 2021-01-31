4a745dd9204170e9cf3a23086adb3df1b7feb7f7
jamescross <crossjames813@gmail.com>
2021-01-31 14:11:01 -0600

    commit tree c78af77f31a7ec7aeb55fc143e75dfb9eb6bb8d4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    str_array = []
+    split_array = self.split(/[^\w\s\,]/)
+    split_array.each do |str|
+      if str.length > 0
+        str_array <<str
+      end
+      
   end
+  str_array.count
+end
 end
